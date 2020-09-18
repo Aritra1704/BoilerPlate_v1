1364d4887b7ec107236e581e57b605e447c818e9
Aritra1704 <aritra1704@gmail.com>
2020-09-18 20:11:57 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Lesson 72
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is boiler plate</p>, document.getElementById('app'));
